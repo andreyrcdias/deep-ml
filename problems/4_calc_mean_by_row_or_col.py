6125abefc3c28b6d92e44483b974e4fc253c9653
Andrey <andreyrcdias@gmail.com>
2025-12-17 18:32:17 -0300

    added more teste cases for problem 4

--- problems/4_calc_mean_by_row_or_col.py
+++ problems/4_calc_mean_by_row_or_col.py
@@ -18,6 +18,8 @@ def calculate_matrix_mean(matrix: list[list[float]], mode: str) -> list[float]:
     [
         ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], "column", [4.0, 5.0, 6.0]),
         ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], "row", [2.0, 5.0, 8.0]),
+        ([[1, 2, 3, 4], [5, 6, 7, 8]], "row", [2.5, 6.5]),
+        ([[1, 2, 3, 4], [5, 6, 7, 8]], "column", [3.0, 4.0, 5.0, 6.0]),
     ],
 )
 def test_calculate_matrix_mean(matrix, mode, expected):
