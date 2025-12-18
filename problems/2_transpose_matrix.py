7fb3c85d3a5774ccd4895103ea9b2cab2e0c0854
Andrey <andreyrcdias@gmail.com>
2025-12-18 09:59:08 -0300

    problem 2 update

--- problems/2_transpose_matrix.py
+++ problems/2_transpose_matrix.py
@@ -14,7 +14,7 @@ def transpose_matrix_v2(a: list[list[int | float]]) -> list[list[int | float]]:
     return [list(col) for col in zip(*a)]
 
 
-def transpose_matrix_v3(a: list[list[int | float]]) -> list[list[int | float]]:
+def transpose_matrix_np(a: list[list[int | float]]) -> list[list[int | float]]:
     import numpy as np
 
     a_np = np.array(a)
