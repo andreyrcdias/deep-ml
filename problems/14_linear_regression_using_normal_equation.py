2c2313af7166766965794d6e61952acb9fcc7b54
Andrey <andreyrcdias@gmail.com>
2025-12-21 18:03:17 -0300

    problem 14 linear regression using normal equation

---
+++ problems/14_linear_regression_using_normal_equation.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pytest
+
+
+def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> list[float]:
+    X = np.array(X)
+    y = np.array(y).reshape(-1, 1)
+
+    X_transpose = X.T
+    theta = np.linalg.inv(X_transpose.dot(X)).dot(X_transpose).dot(y)
+
+    theta = np.round(theta, 4).flatten().tolist()
+    return theta
+
+
+@pytest.mark.parametrize(
+    "X, y, expected",
+    [
+        ([[1, 1], [1, 2], [1, 3]], [1, 2, 3], [0.0, 1.0]),
+        ([[1, 3, 4], [1, 2, 5], [1, 3, 2]], [1, 2, 1], [4.0, -1.0, -0.0]),
+    ],
+)
+def test_calculate_covariance_matrix(X, y, expected):
+    got = linear_regression_normal_equation(X, y)
+    assert got == expected
