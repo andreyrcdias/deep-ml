7b2f7cb29c1c9a2e0ef15a98d08556a6e14abba0
Andrey <andreyrcdias@gmail.com>
2025-12-22 10:48:55 -0300

    problem 14 fix test

--- problems/14_linear_regression_using_normal_equation.py
+++ problems/14_linear_regression_using_normal_equation.py
@@ -20,6 +20,6 @@ def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> l
         ([[1, 3, 4], [1, 2, 5], [1, 3, 2]], [1, 2, 1], [4.0, -1.0, -0.0]),
     ],
 )
-def test_calculate_covariance_matrix(X, y, expected):
+def test_linear_regression_normal_equation(X, y, expected):
     got = linear_regression_normal_equation(X, y)
     assert got == expected
